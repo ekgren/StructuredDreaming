5afc7c6fe7c67b5c87f43cc76c62ac1366e8a197
Ariel Ekgren <ariel.ekgren@gmail.com>
2021-03-11 09:56:14 +0100

    Add setup.py and reqs

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='structureddreaming',
+      version='0.0.1',
+      description='Structured Dreaming',
+      author='Ariel Ekgren',
+      author_email='',
+      url='https://github.com/ekgren/StructuredDreaming',
+      install_requires=[],
+      packages=find_packages(),
+      entry_points={}
+      )
