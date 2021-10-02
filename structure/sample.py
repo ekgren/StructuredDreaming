8100a0ff0ca80d116c305b4adba93aee69e1b11c
Ariel Ekgren <ariel.ekgren@gmail.com>
2021-10-02 17:47:14 +0200

    added stylegan sampler

--- structure/sample.py
+++ structure/sample.py
@@ -1,5 +1,7 @@
 import torch
 
+from .transform import noise
+
 
 @torch.jit.script
 def random_generate_grid(l: int = 2,
@@ -57,3 +59,38 @@ def random_generate_grid(l: int = 2,
         grid_y, grid_x = torch.meshgrid(y, x)
         xy = torch.stack([grid_x, grid_y], dim=2).view(1, l, l, 2) * 2. - 1.
     return xy
+
+
+class ImgSampleStylegan(torch.nn.Module):
+    def __init__(self,
+                 kernel_min: int = 1,
+                 kernel_max: int = 8):
+        super().__init__()
+        self.kernel_min = kernel_min
+        self.kernel_max = kernel_max
+
+    def forward(self, input: torch.Tensor, size: int = 224, bs: int = 1) -> torch.Tensor:
+        rgbs = []
+        for _ in range(bs):
+            kernel_size = int(
+                torch.randint(self.kernel_min, int(torch.randint(self.kernel_min + 1, self.kernel_max, ())), ()).item())
+            grid_size = int(size / 2 + torch.rand(()).item() * size)
+
+            img_out = noise(input)
+            img_out = torch.nn.functional.avg_pool2d(img_out,
+                                                     kernel_size=kernel_size,
+                                                     stride=kernel_size,
+                                                     padding=0)
+
+            mode_idx = int(torch.randint(1, 3, ()).item())
+            mode = ('all', 'even', 'repeat')
+            grid = random_generate_grid(grid_size, mode=mode[mode_idx])
+            img_out = torch.nn.functional.grid_sample(img_out,
+                                                      grid,
+                                                      mode='bilinear',
+                                                      padding_mode='zeros',
+                                                      align_corners=False)
+            img_out = torch.nn.functional.interpolate(img_out, (size, size), mode='area')
+            rgbs.append(img_out)
+
+        return torch.cat(rgbs, dim=0)
