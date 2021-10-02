cd77800cc58b4545ec85a7ab9a39335e4df89ed6
Ariel Ekgren <ariel.ekgren@gmail.com>
2021-10-02 15:15:32 +0200

    Added ClampSGD

--- structure/optim.py
+++ structure/optim.py
@@ -19,3 +19,24 @@ class SimpleSGD(Optimizer):
                 grad = p.grad.data
                 p.data.add_(grad, alpha=-lr)
         return loss
+
+
+class ClampSGD(torch.optim.Optimizer):
+    def __init__(self, params, lr=0.1, clamp=1e-9):
+        defaults = dict(lr=lr, clamp=clamp)
+        super().__init__(params, defaults)
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                lr = group["lr"]
+                clamp = group["clamp"]
+                grad = p.grad.data
+                grad.clamp_(-clamp, clamp).div_(clamp)
+                p.data.add_(grad, alpha=-lr)
+        return loss
