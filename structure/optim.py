d2bf0f980adc34ef3988b67e90042069a17713af
Ariel Ekgren <ariel.ekgren@gmail.com>
2021-10-09 17:54:00 +0200

    updated Clamp SGD with dropout

--- structure/optim.py
+++ structure/optim.py
@@ -4,6 +4,8 @@ from torch.optim import Optimizer
 
 class SimpleSGD(Optimizer):
     def __init__(self, params, lr=0.1):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
         defaults = dict(lr=lr)
         super().__init__(params, defaults)
 
@@ -21,11 +23,24 @@ class SimpleSGD(Optimizer):
         return loss
 
 
-class ClampSGD(Optimizer):
-    def __init__(self, params, lr=0.1, clamp=1e-9):
-        defaults = dict(lr=lr, clamp=clamp)
+class ClampSGD(torch.optim.Optimizer):
+    r""" Clamp SGD
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-1)
+        clamp (float, optional): clamping of gradient (default: 1e-30)
+        drop (float, optional): dropout of gradient (default: 0)
+    """
+    def __init__(self, params, lr=1e-1, clamp=1e-30, drop=0.):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= drop < 1.0:
+            raise ValueError("Invalid dropout value: {}".format(drop))
+        defaults = dict(lr=lr, clamp=clamp, drop=drop)
         super().__init__(params, defaults)
 
+    @torch.no_grad()
     def step(self, closure=None):
         loss = None
         if closure is not None:
@@ -36,7 +51,7 @@ class ClampSGD(Optimizer):
                     continue
                 lr = group["lr"]
                 clamp = group["clamp"]
-                grad = p.grad.data
-                grad.clamp_(-clamp, clamp).div_(clamp)
+                drop = group["drop"]
+                grad = torch.nn.functional.dropout(p.grad.data.clamp_(-clamp, clamp).div_(clamp), p=drop)
                 p.data.add_(grad, alpha=-lr)
         return loss
